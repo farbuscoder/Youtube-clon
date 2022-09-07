2f4bc2b04393db173ca6c298517d2c271aea9cbc
FarBusC4 <marcosfer243@gmail.com>
2022-09-06 22:59:16 -0300

    VIdeo methods part one

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,6 +1,34 @@
 import express from "express";
-import {} from "../controllers/video.js";
+import {
+  addVideo,
+  updateVideo,
+  deleteVideo,
+  getVideo,
+} from "../controllers/video.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
+
+//create a video
+router.post("/", verifyToken, addVideo);
+
+//Update a video
+
+router.put("/:id", verifyToken, updateVideo);
+
+//Delete a video
+
+router.delete("/:id", verifyToken, deleteVideo);
+
+//Get a video
+
+router.get("/find/:id", getVideo);
+
+//Increase Views
+
+router.put("/view");
+router.put("/trend");
+router.put("/random");
+router.put("/sub");
 
 export default router;
