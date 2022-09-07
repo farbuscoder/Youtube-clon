568f7b1fb0ccd7521df5876efcebe74813ae5f53
FarBusC4 <marcosfer243@gmail.com>
2022-09-06 22:57:48 -0300

    User methods

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import { createError } from "../error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   try {
@@ -30,6 +31,17 @@ export const signin = async (req, res, next) => {
     if (!isCorrect) {
       return next(createError(404, "Wrong Credentials!"));
     }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT);
+
+    const { password, ...others } = user._doc;
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(others);
   } catch (err) {
     next(createError(404, "Not Found"));
   }
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,76 @@
-export const test = (req, res) => {
-  res.json("It is successful");
+import User from "../models/User.js";
+import { createError } from "../error.js";
+
+export const update = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json("User has been deleted.");
+    } catch (err) {
+      next(err);
+    }
+  } else {
+    return next(createError(404, "You can update only your account"));
+  }
 };
+
+export const deleteUser = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("User has been deleted");
+    } catch (err) {
+      next(err);
+    }
+  } else {
+    return next(createError(404, "You can delete only your account"));
+  }
+};
+export const getUser = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      res.status(200).json(user);
+    } catch (err) {
+      next(err);
+    }
+  }
+};
+export const subscribe = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      await User.findById(req.user.id, {
+        $push: { subscribedUsers: req.params.id },
+      });
+      await User.findByIdAndUpdate(req.params.id, {
+        $inc: { subscribers: 1 },
+      });
+      res.status(200).json("Subscription successful");
+    } catch (err) {
+      next(err);
+    }
+  }
+};
+export const unsubscribe = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      await User.findById(req.user.id, {
+        $pull: { subscribedUsers: req.params.id },
+      });
+      await User.findByIdAndUpdate(req.params.id, {
+        $inc: { subscribers: -1 },
+      });
+      res.status(200).json("Unsubscription successful");
+    } catch (err) {
+      next(err);
+    }
+  }
+};
+export const like = (req, res, next) => {};
+export const dislike = (req, res, next) => {};
