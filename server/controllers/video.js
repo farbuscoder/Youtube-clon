6e144c8b0058409a396a24d9fec9ee90c31197f0
FarBusC4 <marcosfer243@gmail.com>
2022-10-05 22:10:44 -0300

    AddView, trend, random and sub methods

--- server/controllers/video.js
+++ server/controllers/video.js
@@ -77,3 +77,27 @@ export const addView = async (req, res, next) => {
     res.status(200).json(video);
   } catch (error) {}
 };
+
+export const random = async (req, res, next) => {
+  try {
+    const video = await Video.findById(req.params.id);
+
+    res.status(200).json(video);
+  } catch (error) {}
+};
+
+export const trend = async (req, res, next) => {
+  try {
+    const video = await Video.findById(req.params.id);
+
+    res.status(200).json(video);
+  } catch (error) {}
+};
+
+export const sub = async (req, res, next) => {
+  try {
+    const video = await Video.findById(req.params.id);
+
+    res.status(200).json(video);
+  } catch (error) {}
+};
